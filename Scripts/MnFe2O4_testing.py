2ad758f174f24afd1d025af47f4b58eb13122fbf
robert1471 <robertcdickson.2018@gmail.com>
2020-08-14 15:01:10 +0100

    updated MnFe2O4_testing.py

--- Scripts/MnFe2O4_testing.py
+++ Scripts/MnFe2O4_testing.py
@@ -1,6 +1,5 @@
 from ase.calculators.vasp import Vasp
 from ase.io import read
-from ase.dft.kpoints import bandpath
 import matplotlib.pyplot as plt
 import os
 import shutil
@@ -41,22 +40,22 @@ class VaspCalculations(object):
                                     "prec": 'accurate',
                                     "lorbit": 11}
 
-        self.relax_calculation = {"ibrion": 2,  # determines how ions are moved and updated (MD or relaxation)
+        self.relax = {"ibrion": 2,  # determines how ions are moved and updated (MD or relaxation)
                                   "nsw": 50,  # number of ionic steps
                                   "isif": 3}
 
-        self.scf_calculation = {"icharg": 2}
+        self.scf = {"icharg": 2}
 
-        self.bands_calculation = {"kpts": band_k_path().kpts, "icharg": 11}
+        #self.bands = {"kpts": band_k_path().kpts, "icharg": 11}
 
-        self.eps_calculation = {"algo": "exact",
+        self.eps = {"algo": "exact",
                                 "loptics": True,
                                 "nbands": "lots",
                                 "nedos": 1000}
 
-        self.hse_calculation = {"xc": "HSE06"}
+        self.hse = {"xc": "HSE06"}
 
-        self.hubbard_calculation = {"ldau": True,
+        self.hubbard = {"ldau": True,
                                     "ldau_luj": hubbard_parameters,
                                     "ldauprint": 2}
 
@@ -66,10 +65,10 @@ class VaspCalculations(object):
                                 "ecut": {},
                                 }
         self.calculation_parameters = {"general": self.general_calculation,
-                                       "scf": self.general_calculation.update(self.scf_calculation),
-                                       "bands": self.general_calculation.update(self.bands_calculation),
-                                       "eps": self.general_calculation.update(self.eps_calculation),
-                                       "hse06": self.general_calculation.update(self.hse_calculation)}
+                                       "scf": self.general_calculation.update(self.scf),
+                                       # "bands": self.general_calculation.update(self.bands_calculation),
+                                       "eps": self.general_calculation.update(self.eps),
+                                       "hse06": self.general_calculation.update(self.hse)}
 
         self.structure = structure
         self.calculations = calculations
@@ -99,7 +98,7 @@ class VaspCalculations(object):
             energies = []
 
             # append the general VASP keywords for the test
-            vasp_keywords = self.general_calculation.update(self.test_parameters[test])
+            vasp_keywords = self.general_calculation.copy()
 
             for test_value in values:
                 test_path = "./{}".format(test_value)
@@ -115,16 +114,17 @@ class VaspCalculations(object):
                 elif test == "ecut":
                     test_variable = "ecut"
                 else:
-                    raise ValueError('Error: test requested is not implemented. Please check test argument and try again.')
+                    raise ValueError('Error: test requested is not implemented. Please check test argument and try '
+                                     'again.')
 
                 # update keywords dictionary to have new test value
-                vasp_keywords = vasp_keywords.update({test_variable: test_value})
+                vasp_keywords.update({test_variable: test_value})
 
-                calculation = self.structure.set_calculator(Vasp(**vasp_keywords))
+                self.structure.set_calculator(Vasp(**vasp_keywords))
                 try:
-                    energy = calculation.get_potential_energy()
+                    energy = self.structure.get_potential_energy()
                     energies.append(energy)
-                except:
+                except (TypeError, ValueError):
                     print("A VASP error has occurred in test: {}|{}. Please check again".format(test, test_value))
                     energies.append("")
                 os.chdir("../")
@@ -136,20 +136,11 @@ class VaspCalculations(object):
 
         return energies
 
-
-
-        # for calculation in self.calculations:
-        # TODO: Who calculation workflow
-        #    print("test")
-
-    def structure_initialise(self):
-        print("initialise structures")
-
-    def single_vasp_calculation(self, main_settings, additional_settings, restart=False):
+    def single_vasp_calculation(self, calculation_type, main_settings, additional_settings, restart=False):
         with open(self.output_file, "w") as vasp_out:
             # defining vasp settings
             vasp_settings = main_settings.copy()
-            vasp_settings.update(additional_settings)
+            vasp_settings.update(self.calculation_type)
 
             if os.path.isfile("OSZICAR"):
                 with open("OSZICAR", mode="r") as oszicar_file:
@@ -159,5 +150,5 @@ class VaspCalculations(object):
 MnFe2O4_structure = read("./Cifs/MnFe2O4-Normal.cif")
 MnFe2O4_tests = VaspCalculations(MnFe2O4_structure)
 
-k_test = MnFe2O4_tests.test_manager("k-points", [1, 2, 3])
+k_test = MnFe2O4_tests.parameter_testing("k-points", [1, 2, 3])
 
