341d2d59bc0ff4d576737c4b3a3a2a803e095b28
Robert <robertcdickson.2018@gmail.com>
2020-08-17 10:06:58 +0100

    Updated MnFe2O4_testing.py

--- Scripts/MnFe2O4_testing.py
+++ Scripts/MnFe2O4_testing.py
@@ -42,23 +42,23 @@ class VaspCalculations(object):
                                     "lorbit": 11}
 
         self.relax = {"ibrion": 2,  # determines how ions are moved and updated (MD or relaxation)
-                                  "nsw": 50,  # number of ionic steps
-                                  "isif": 3}
+                      "nsw": 50,  # number of ionic steps
+                      "isif": 3}
 
         self.scf = {"icharg": 2}
 
-        #self.bands = {"kpts": band_k_path().kpts, "icharg": 11}
+        # self.bands = {"kpts": band_k_path().kpts, "icharg": 11}
 
         self.eps = {"algo": "exact",
-                                "loptics": True,
-                                "nbands": "lots",
-                                "nedos": 1000}
+                    "loptics": True,
+                    "nbands": "lots",
+                    "nedos": 1000}
 
-        self.hse = {"xc": "HSE06"}
+        self.hse06 = {"xc": "HSE06"}
 
         self.hubbard = {"ldau": True,
-                                    "ldau_luj": hubbard_parameters,
-                                    "ldauprint": 2}
+                        "ldau_luj": hubbard_parameters,
+                        "ldauprint": 2}
 
         self.structure = structure
         self.calculations = calculations
@@ -67,9 +67,15 @@ class VaspCalculations(object):
 
     def parameter_testing(self, test, values):
         # ------------------------------------------------------------------ #
-        #  master function that is used to call all other functions in turn  #
+        #  This function allows for testing of many different values for a   #
+        #                          given test                                #
         # ------------------------------------------------------------------ #
 
+        # TODO: Make option to plot and save figure of convergence test
+        # TODO: At the moment, output is only written at end of all calculations as with open statement ends
+        #   This needs to be amended so output is written as each calculation finishes
+
+        # with open statement allows for continuous writing of output
         with open(self.output_file, "a+") as out_file:
 
             out_file.write("--------------------\n")
@@ -84,6 +90,7 @@ class VaspCalculations(object):
 
             out_file.write("Testing calculations running from the directory: {} \n".format(path_name))
             out_file.write("{} | Energy \n".format(test))
+
             # list for storage of tested output
             energies = []
 
@@ -110,14 +117,17 @@ class VaspCalculations(object):
                 # update keywords dictionary to have new test value
                 vasp_keywords.update({test_variable: test_value})
 
+                # set calculator
                 self.structure.set_calculator(Vasp(**vasp_keywords))
+
+                # run calculation
                 try:
                     energy = self.structure.get_potential_energy()
-
                 except (TypeError, ValueError):
                     print("A VASP error has occurred in test: {} | {}. Please check again".format(test, test_value))
                     energy = 0
                 out_file.write("| {} | {} | \n".format(test_value, energy))
+
                 energies.append(energy)
                 os.chdir("../")
 
@@ -125,19 +135,26 @@ class VaspCalculations(object):
 
         return energies
 
-    def single_vasp_calculation(self, calculation_type, main_settings, additional_settings, restart=False):
-        with open(self.output_file, "w") as vasp_out:
+    def single_vasp_calculation(self, calculation_type, additional_settings, restart=False):
+        with open(self.output_file, "a+") as vasp_out:
             # defining vasp settings
-            vasp_settings = main_settings.copy()
-            vasp_settings.update(self.calculation_type)
+            vasp_settings = self.general_calculation.update(calculation_type)
+            vasp_settings.update(additional_settings)
 
             if os.path.isfile("OSZICAR"):
                 with open("OSZICAR", mode="r") as oszicar_file:
                     shutil.copyfileobj(oszicar_file, vasp_out)
 
+    # TODO: Self-consistent hubbard set-up
+    # TODO: Calculation Manager
+    # TODO: single calculations for relaxation, scf, bands, eps,
+    # TODO: single calculations for HSE06, SCAN, GGA+U,
+
 
 MnFe2O4_structure = read("../Cifs/MnFe2O4-Normal.cif")
 MnFe2O4_tests = VaspCalculations(MnFe2O4_structure)
 
-k_test = MnFe2O4_tests.parameter_testing("k-points", [1, 2, 3])
-
+# k testing
+# k_test = MnFe2O4_tests.parameter_testing("k-points", [1, 2, 3])
+# ecut testing
+# ecut_test = MnFe2O4_tests.parameter_testing("ecut", [400, 450, 500, 550, 600, 650, 700, 750, 800])
